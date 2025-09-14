34e1eca5bf719a74fb425e56a140c157a4680577
fortunengili <ngilifofs@gmail.com>
2025-09-14 22:47:14 +0300

    tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('#create-task-form');
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    const input = document.querySelector('#new-task-description');
+    makeToDo(input.value);
+    form.reset();
+  })
 });
+function makeToDo(myTodo){
+  const li = document.createElement('li');
+  const btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  btn.className = 'button'
+  btn.textContent = 'x' 
+  btn.style.marginLeft = "10px"
+  li.textContent = `${myTodo}`
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
